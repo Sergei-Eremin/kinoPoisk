bbde7b0b3f6d0f4215c72abb2ad0dadc6a75d247
Nikita Gerasimov <dok-strelok@yandex.ru>
2024-10-12 21:07:19 +0400

    init

---
+++ backend/main/java/com/garage/kinopoisk/KinoPoiskApplication.java
@@ -0,0 +1,13 @@
+package com.garage.kinopoisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoPoiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoPoiskApplication.class, args);
+    }
+
+}
